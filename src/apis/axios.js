54cf4d82df1bf3843b8d0eb6da27c3c538c55a37
Jaewon <tjrwodnjs99@gmail.com>
2023-06-17 17:17:51 +0900

    .

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -2,17 +2,16 @@ import axios from "axios";
 import { getCookie } from "../utils/cookie";
 import { refreshToken } from "./api";
 
-axios.defaults.baseURL = "http://localhost:8000/api";
-axios.defaults.withCredentials = true;
-axios.defaults.headers.post["Content-Type"] = "application/json";
-axios.defaults.headers.common["X-CSRFToken"] = getCookie("csrftoken");
-
 if (process.env.NODE_ENV === "development") {
   axios.defaults.baseURL = "http://localhost:8000/api";
 } else {
   axios.defaults.baseURL =
     "https://port-0-snulion-week12-koh2xlisex7sv.sel4.cloudtype.app/api";
 }
+
+axios.defaults.withCredentials = true;
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.common["X-CSRFToken"] = getCookie("csrftoken");
 
 // 누구나 접근 가능한 API들
 export const instance = axios.create();
